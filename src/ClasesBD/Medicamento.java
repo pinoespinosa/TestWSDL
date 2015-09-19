2373aa4bf2ac096dc9411aaf865dfe92bd6d84a7
Pino <pino.espinosa@gmail.com>
2015-09-19 09:07:50 -0300

    refactoring

--- src/ClasesBD/Medicamento.java
+++ src/ClasesBD/Medicamento.java
@@ -1,6 +1,9 @@
 package ClasesBD;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import Web.Testing;
 import Web.managerDB;
@@ -13,6 +16,61 @@ public class Medicamento {
 		if(!nombreList.isEmpty())
 			nombreDrog=nombreList.get(0);
 		return nombreDrog;
+	}
+
+	/**
+	 *  Retorna el idSnomed de la forma farmaceutica simplificada del medicamento
+	 * @param idManfar
+	 * @return
+	 */
+	public static String generateFormaFarmaceutica(String idManfar){
+		String id_FormaFa="%";
+
+		// Busco el id del medicamento y entre todos los sinonimos hallo su formaFarmaceutica
+
+		// Conjunto de formasFarmaceuticas, (algunas son del aleman, otras null)
+		Set<String> formas = new HashSet<String>();
+		formas.addAll(managerDB.executeScript_Query("SELECT formaFarmaceutica as formFarm  FROM "+Testing.esquema+".medicamentos_manfar WHERE id_manfar = '"+idManfar+"';","formFarm"));
+
+		// Transforma las descripciones a id, sacando las invalidas o vacias
+		List<String> formasFiltradas= new ArrayList<String>();
+		for (String string : formas) {
+			String aux = FormaFarmaceutica.descrip_to_idFormFarmBasic(string);
+			if (!aux.equals(""))
+				formasFiltradas.add(aux);
+		}
+
+		// Guardo el id de la formaFarmaceutica
+		if (!formasFiltradas.isEmpty())
+			id_FormaFa=formasFiltradas.get(0);
+		
+		return id_FormaFa;
+	}
+
+	/**
+	 * Retorna la cantidad de unidades o el simbolo porcentaje
+	 * @param idManfar
+	 * @return
+	 */
+	public static String generateUnidades (String idManfar){
+		String 	unidad="%";
+
+		// Conjunto de formasFarmaceuticas, (algunas son del aleman, otras null)
+		Set<String> unidades = new HashSet<String>();
+		unidades.addAll(managerDB.executeScript_Query("SELECT unidades  FROM "+Testing.esquema+".medicamentos_manfar WHERE id_manfar = '"+idManfar+"';","unidades"));
+
+		// Transforma las descripciones a id, sacando las invalidas o vacias
+		List<String> unidadesFiltradas= new ArrayList<String>();
+		for (String string : unidades) {
+			if (!string.equals("0"))
+				unidadesFiltradas.add(string);
+		}
+
+		// Guardo el id de la formaFarmaceutica
+		if (!unidadesFiltradas.isEmpty())
+			unidad=unidadesFiltradas.get(0);
+		
+		return unidad;
 	}
 
 }
--- src/Web/BuscadorMedicamentos.java
+++ src/Web/BuscadorMedicamentos.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Set;
 
 import xml.EventoAmbiguedad;
-import ClasesBD.FormaFarmaceutica;
+import ClasesBD.Medicamento;
 import ClasesBD.Script;
 
 
@@ -36,13 +36,13 @@ public class BuscadorMedicamentos {
 
 		if (Integer.parseInt(id)>=0)
 		{
-			// Busco si esta referenciado directametne
+			// Busco si esta referenciado directamente
 			resultado.addAll(buscarMedRefDirecto(id));
 
 			if (!hallado)
 				resultado.addAll(buscarCacheDeBusquedas(id));
 
-			if ( !hallado && ( !cacheNull || BuscadorMedicamentos.isFlagEventoAmbiguedadOFF()) ){
+			if ( !hallado || cacheNull ){
 				resultado.addAll(buscarMedPorCaract(id));}
 
 			if (!hallado)
@@ -85,47 +85,15 @@ public class BuscadorMedicamentos {
 	}
 	private static Set<String> buscarMedPorCaract(String id){
 
+
+		// ****************************** Busco el/los medicamento ******************************
+
+		String 	id_FormaFa=Medicamento.generateFormaFarmaceutica(id),
+				unidad=Medicamento.generateUnidades(id);
+
 		List<String> hallados = new ArrayList<String>();
-		Set<String> resultado = new HashSet<String>();
-
-		String id_FormaFa="%", unidad="%";
-
-		// Busco el id del medicamento y entre todos los sinonimos hallo su formaFarmaceutica
-
-		// Conjunto de formasFarmaceuticas, (algunas son del aleman, otras null)
-		Set<String> formas = new HashSet<String>();
-		formas.addAll(managerDB.executeScript_Query("SELECT formaFarmaceutica as formFarm  FROM "+Testing.esquema+".medicamentos_manfar WHERE id_manfar = '"+id+"';","formFarm"));
-
-		// Transforma las descripciones a id, sacando las invalidas o vacias
-		List<String> formasFiltradas= new ArrayList<String>();
-		for (String string : formas) {
-			String aux = FormaFarmaceutica.descrip_to_idFormFarmBasic(string);
-			if (!aux.equals(""))
-				formasFiltradas.add(aux);
-		}
-
-		// Guardo el id de la formaFarmaceutica
-		if (!formasFiltradas.isEmpty())
-			id_FormaFa=formasFiltradas.get(0);
-
-
-		// UNIDADES
-
-		// Conjunto de formasFarmaceuticas, (algunas son del aleman, otras null)
-		Set<String> unidades = new HashSet<String>();
-		unidades.addAll(managerDB.executeScript_Query("SELECT unidades  FROM "+Testing.esquema+".medicamentos_manfar WHERE id_manfar = '"+id+"';","unidades"));
-
-		// Transforma las descripciones a id, sacando las invalidas o vacias
-		List<String> unidadesFiltradas= new ArrayList<String>();
-		for (String string : unidades) {
-			if (!string.equals("0"))
-				unidadesFiltradas.add(string);
-		}
-
-		// Guardo el id de la formaFarmaceutica
-		if (!unidadesFiltradas.isEmpty())
-			unidad=unidadesFiltradas.get(0);
-
+		
+		// Busco los medicamentos y si no se hallaron relajo el nombre
 		hallados.addAll(managerDB.executeScript_Query(Script.getMedCarateristicasWithNombre(id, id_FormaFa, unidad),"nombre"));		
 
 		if(hallados.isEmpty()){
@@ -133,31 +101,89 @@ public class BuscadorMedicamentos {
 		}		
 
 
+		// ****************************** Lo guardo en el cache **********************************
 
-		//-------------------------- DECISION *--------------------------------------------
-
-		if (hallados.isEmpty())
-		{
+		if (hallados.isEmpty()){
 			managerDB.executeScript_Void("INSERT INTO "+Testing.esquema+".`cache_busquedas`VALUES ('"+id+"','null');");
 		}
 		else{
+			int i=0;
 			for (String string : hallados) {
-				if (string .length() <250)
-					managerDB.executeScript_Void("INSERT INTO "+Testing.esquema+".`cache_busquedas`VALUES ('"+id+"','"+string+"');");
+				if (string .length() <2000)
+					managerDB.executeScript_Void("INSERT INTO "+Testing.esquema+".`cache_busquedas`VALUES ('"+id+"','"+i+"','"+string+"');");
+				i++;
 			}
-
-
-		}
-
-		if (!hallados.isEmpty()){
 			hallado=true;
 			System.out.println("Caracteristicas total:" + Script.getDrogasCantidad(hallados.get(0)));
 		}
+		
+
+		// ****************************** Busco las drogas **********************************
+		
+		Set<String> resultado =hallarDrogas(hallados, id, "Carac");
 
 
+		return resultado;
+	}
+
+	private static Set<String> buscarCacheDeBusquedas(String id) {
+		List<String> hallados = managerDB.executeScript_Query( "SELECT data_anmat FROM `" + Testing.esquema	+ "`.cache_busquedas WHERE idmanfar='" + id + "';", "data_anmat");
+
+		if (!hallados.isEmpty()) {
+			hallado = true;
+		}
+
+		Set<String> resultado = null;
+		if (hallado)
+			if (hallados.get(0).equals("null")) {
+				System.out.println("Cache: null\n");
+				cacheNull = true;
+			} else {
+				resultado = hallarDrogas(hallados, id, "Cache");
+			}
+
+		return resultado;
+
+	}
+	
+	public static void createEventoAmbiguedad(List<String> hallados, String id){
+		
+		Testing.patientBankData=null;
+		Testing.bufferAmbiguedades= new EventoAmbiguedad[hallados.size()];
+		for (int i=0;i<hallados.size();i++) 
+		{
+			Testing.bufferAmbiguedades[i] = new EventoAmbiguedad();			
+
+			String descripcion_drogas = "[ ";
+
+			List<String> info = managerDB.executeScript_Query(
+					"	SELECT DISTINCT anm.drogas_texto as cant"
+							+ "	FROM 	"
+							+Testing.esquema+".medicamentos_manfar as man, "
+							+Testing.esquema+".drogas_anmat as anm		"
+							+ "	WHERE "
+							+ "	man.nombre ="+id+" and "
+							+ "	anm.nro_certificado_anmat="+hallados.get(i).split("_")[0] +" and "
+							+ "	anm.indexCertif="+hallados.get(i).split("_")[1],"cant");
+
+			for (String string : info) {
+				descripcion_drogas+= string + " / ";
+			}
+
+			descripcion_drogas+="]";
+
+			Testing.bufferAmbiguedades[i].setData(id, hallados.get(i).split("_")[2], hallados.get(i).split("_")[3] + " " + descripcion_drogas, hallados.get(i).split("_")[0]+"_"+hallados.get(i).split("_")[1]  );
+
+		}
+	}
+	
+	public static Set<String> hallarDrogas(List<String> hallados, String id, String contexto){
+		
+		Set<String> resultado = new HashSet<String>();
+		
 		if (hallados.size()==1){
 			resultado.addAll(managerDB.executeScript_Query(Script.getDrogas(hallados.get(0)),"nombre"));
-			System.out.println("Caracteristicas halladas: "+resultado.size()+"\n");
+			System.out.println(contexto + " halladas: "+resultado.size()+"\n");
 		}
 		else{
 			// Puede que haya o muchos o ninguno
@@ -166,142 +192,24 @@ public class BuscadorMedicamentos {
 				// Dado que hay varios verifico si el profesional ya cargo una sugenrencia de cual medicamento se trata
 				List<String> sugerencia = new ArrayList<String>();
 				sugerencia.addAll(managerDB.executeScript_Query("SELECT concat(id_certif_anmat,'_',id_index_anmat) as med FROM "+Testing.esquema+".medicamentos_manfar_sugerencias WHERE id_medico='"+Testing.patientBankData.getIdDoctor()+"' && id_manfar='"+id+"';", "med"));
-
+				
+				resultado.clear();
 				if (sugerencia.isEmpty())
 				{
-					Testing.patientBankData=null;
-					Testing.bufferAmbiguedades= new EventoAmbiguedad[hallados.size()];
-					for (int i=0;i<hallados.size();i++) 
-					{
-						Testing.bufferAmbiguedades[i] = new EventoAmbiguedad();			
-
-						String descripcion_drogas = "[ ";
-
-						List<String> info = managerDB.executeScript_Query(
-								"	SELECT DISTINCT anm.drogas_texto as cant"
-										+ "	FROM 	"
-										+Testing.esquema+".medicamentos_manfar as man, "
-										+Testing.esquema+".drogas_anmat as anm		"
-										+ "	WHERE "
-										+ "	man.nombre ="+id+" and "
-										+ "	anm.nro_certificado_anmat="+hallados.get(i).split("_")[0] +" and "
-										+ "	anm.indexCertif="+hallados.get(i).split("_")[1],"cant");
-
-						for (String string : info) {
-							//	System.out.println(string);
-							descripcion_drogas+= string + " / ";
-						}
-
-						descripcion_drogas+="]";
-
-						Testing.bufferAmbiguedades[i].setData(id, hallados.get(i).split("_")[2], hallados.get(i).split("_")[3] + " " + descripcion_drogas, hallados.get(i).split("_")[0]+"_"+hallados.get(i).split("_")[1]  );
-
-					}
+					createEventoAmbiguedad(hallados,id);
 				}
 				else
 				{
 					hallados.clear();
 					resultado.addAll(managerDB.executeScript_Query(Script.getDrogas(sugerencia.get(0)),"nombre"));	
-					System.out.println("Caracteristicas halladas: "+resultado.size()+"\n");
+					System.out.println(contexto + "halladas: "+resultado.size()+"\n");
 				}
 
 			}	
 		}
 		return resultado;
 	}
-	private static Set<String> buscarCacheDeBusquedas(String id){
-		List<String> hallados = managerDB.executeScript_Query("SELECT data_anmat FROM `"+Testing.esquema+"`.cache_busquedas WHERE idmanfar='"+id+"';", "data_anmat");
+	
 
-		Set<String> resultado = new HashSet<String>();
-
-		if (!hallados.isEmpty()){
-			hallado=true;
-		}
-
-
-		if (hallado && !hallados.get(0).equals("null"))
-		{ 
-			System.out.println("Cache:" + Script.getDrogasCantidad(hallados.get(0)));
-
-			//	for (String string : hallados) {
-			//		System.out.println(string);
-			//		}
-
-			if (hallados.size()==1)
-			{
-				resultado.addAll(managerDB.executeScript_Query(Script.getDrogas(hallados.get(0)),"nombre"));	
-				System.out.println("Cache hallados: "+resultado.size()+"\n");
-			}
-			else{
-				// Puede que haya o muchos o ninguno
-				if (hallados.size()>1)
-				{
-					// Dado que hay varios verifico si el profesional ya cargo una sugenrencia de cual medicamento se trata
-					List<String> sugerencia = new ArrayList<String>();
-					sugerencia.addAll(managerDB.executeScript_Query("SELECT concat(id_certif_anmat,'_',id_index_anmat) as med FROM "+Testing.esquema+".medicamentos_manfar_sugerencias WHERE id_medico='"+Testing.patientBankData.getIdDoctor()+"' && id_manfar='"+id+"';", "med"));
-
-					if (sugerencia.isEmpty())
-					{
-						Testing.patientBankData=null;
-						Testing.bufferAmbiguedades= new EventoAmbiguedad[hallados.size()];
-						for (int i=0;i<hallados.size();i++) 
-						{
-							Testing.bufferAmbiguedades[i] = new EventoAmbiguedad();			
-							String descripcion_drogas = "[ ";
-
-							List<String> info = managerDB.executeScript_Query(
-									"	SELECT DISTINCT anm.drogas_texto as cant"
-											+ "	FROM 	"
-											+Testing.esquema+".medicamentos_manfar as man, "
-											+Testing.esquema+".drogas_anmat as anm		"
-											+ "	WHERE "
-											+ "	man.id_manfar ="+id+" and "
-											+ "	anm.nro_certificado_anmat="+hallados.get(i).split("_")[0] +" and "
-											+ "	anm.indexCertif="+hallados.get(i).split("_")[1],"cant");
-
-							for (String string : info) {
-								//		System.out.println(string);
-								descripcion_drogas+= string + " - ";
-							}
-							descripcion_drogas+="]";
-
-							Testing.bufferAmbiguedades[i].setData(id, hallados.get(i).split("_")[2], hallados.get(i).split("_")[3] + " " + descripcion_drogas, hallados.get(i).split("_")[0]+"_"+hallados.get(i).split("_")[1]  );
-
-						}
-						resultado.clear();
-						System.out.println();
-					}
-					else
-					{
-						resultado.clear();
-						resultado.addAll(managerDB.executeScript_Query(Script.getDrogas(sugerencia.get(0)),"nombre"));	
-						System.out.println("Cache hallados: "+resultado.size()+"\n");
-					}
-
-				}
-				//		else
-				//			System.out.println("Cache hallados: 0\n");
-			}
-
-
-
-		}
-
-		//	else
-		//	if (hallado && hallados.get(0).equals("null"))
-		//		System.out.println("Cache: null\n"); 
-
-		else
-		{
-
-			if (hallado && hallados.get(0).equals("null"))
-				cacheNull=true;
-
-		}
-
-		return resultado;
-
-
-	}
 
 }
